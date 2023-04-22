ff7678cbbb5b4e388ce3fb0d3fb8ec6aeac7efc2
r0ushann <roushan007kr@gmail.com>
2023-04-22 11:53:04 +0530

    added components folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,16 @@
-
+import { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios";
+import logo from './logo.png';
+import {Layout, Button} from "antd";
+import {useConnect, useAccount, useDisconnect} from "wagmi";
+import {MetaMaskConnector} from "wagmi/connectors/metaMask";
+import CurrentBalance from "./components/CurrentBalance";
+import AccountDetails from "./components/AccountDetails";
+import RecentActivity from "./components/RecentActivity";
+import RequestAndPay from "./components/RequestAndPay";
+
+const { Header, Content} = Layout;
 
 function App() {
   return (
