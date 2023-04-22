64d01467545db549d160aae8397f94fe60c033eb
r0ushann <roushan007kr@gmail.com>
2023-04-23 00:36:52 +0530

    backend functions added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,121 @@
-import logo from './logo.png';
-// import pay from './pay.png';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import logo from "./logo.png";
+import con from "./connect.gif"
 import { Layout, Button } from "antd";
 import CurrentBalance from "./components/CurrentBalance";
 import RequestAndPay from "./components/RequestAndPay";
 import AccountDetails from "./components/AccountDetails";
 import RecentActivity from "./components/RecentActivity";
+import { useConnect, useAccount, useDisconnect } from "wagmi";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import axios from "axios";
 
-const {Header, Content} = Layout;
+const { Header, Content } = Layout;
 
 function App() {
+  const { address, isConnected } = useAccount();
+  const { disconnect } = useDisconnect();
+  const { connect } = useConnect({
+    connector: new MetaMaskConnector(),
+  });
+
+  const [name, setName] = useState("...");
+  const [balance, setBalance] = useState("...");
+  const [dollars, setDollars] = useState("...");
+  const [history, setHistory] = useState(null);
+  const [requests, setRequests] = useState({ "1": [0], "0": [] });
+
+  function disconnectAndSetNull() {
+    disconnect();
+    setName("...");
+    setBalance("...");
+    setDollars("...");
+    setHistory(null);
+    setRequests({ "1": [0], "0": [] });
+  }
+
+  async function getNameAndBalance() {
+    const res = await axios.get(`http://localhost:3001/getNameAndBalance`, {
+      params: { userAddress: address },
+    });
+
+    const response = res.data;
+    console.log(response.requests);
+    if (response.name[1]) {
+      setName(response.name[0]);
+    }
+    setBalance(String(response.balance));
+    setDollars(String(response.dollars));
+    setHistory(response.history);
+    setRequests(response.requests);
+    
+  }
+
+  useEffect(() => {
+    if (!isConnected) return;
+    getNameAndBalance();
+  }, [isConnected]);
+
   return (
     <div className="App">
       <Layout>
-      <Header className="header">
-        <div className="leftHeader">
-         <img src={logo} alt="logo" />    
-          <>
-          <div className='menuOption'>Summary</div>
-          <div className='menuOption'>Activity</div>
-          <div className='menuOption'>{`Send & Request`}</div>
-          <div className='menuOption'>Wallet</div>
-          <div className='menuOption'>Help</div>
-          </>
-        </div>
-        <Button type={"primary"}>Connect Wallet</Button>
-      </Header>
-      <Content className="content">
-          <div className="firstColumn">
-            <CurrentBalance />
-            <RequestAndPay />
-            <AccountDetails />
+        <Header className="header">
+          <div className="leftHeader">
+            <img src={logo} alt="logo" className="logo" />
+            {isConnected && (
+              <>
+                <div
+                  className="menuOption"
+                  style={{ borderBottom: "1.5px solid black" }}
+                >
+                  Summary
+                </div>
+                <div className="menuOption">Activity</div>
+                <div className="menuOption">{`Send & Request`}</div>
+                <div className="menuOption">Wallet</div>
+                <div className="menuOption">Help</div>
+              </>
+            )}
           </div>
-          <div className="secondColumn">
-            <RecentActivity />
-          </div>
+          {isConnected ? (
+            <Button type={"primary"} onClick={disconnectAndSetNull}>
+              Disconnect Wallet
+            </Button>
+          ) : (
+            <Button type={"primary"} onClick={()=>{
+              console.log(requests); connect();
+            }}>
+              Connect Wallet
+            </Button>
+          )}
+        </Header>
+        <Content className="content">
+          {isConnected ? (
+            <>
+              <div className="firstColumn">
+                <CurrentBalance dollars={dollars} />
+                <RequestAndPay requests={requests} getNameAndBalance={getNameAndBalance}/>
+                <AccountDetails
+                  address={address}
+                  name={name}
+                  balance={balance}
+                />
+              </div>
+              <div className="secondColumn">
+                <RecentActivity history={history} />
+              </div>
+            </>
+          ) : (
+            
+              <div className="pleaseLogin">
+                
+                please click the Connect Button to proceed!
+                <img src={con} alt="connect"/>
+               
+            </div>
+            
+          )}
         </Content>
       </Layout>
     </div>
